aa95ad6a97719c1ca74948d41a2b74b919b4b612
maxhou <maxhou@tencent.com>
2022-01-08 16:20:57 +0800

    Feature prepare test os

--- cicd/prepare_tlinux2.2.sh
+++ cicd/prepare_tlinux2.2.sh
@@ -11,18 +11,21 @@ if ! type docker >/dev/null 2>&1; then
   sudo ip link set dev docker0 up
   ip addr show docker0
 
-  echo "start docker"
-
   echo "change docker storage location"
   cp ./daemon.json /etc/docker/
 
+  echo "start docker"
   systemctl daemon-reload
   systemctl start docker
   systemctl enable docker
 fi
 
+echo "check git"
+if ! type git >/dev/null 2>&1; then
+   yum install git -y
+fi
 
 # ubuntu SUSE redhat centos
-docker pull centos:centos7
+#docker pull centos:centos7
 
 
